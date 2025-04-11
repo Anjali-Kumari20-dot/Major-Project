8f5da6e4b9682aa0f8a2325c25b1f3e3c3b79cba
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-04-11 21:46:30 +0530

    sessions

--- WanderLust/classRoom/server.js
+++ WanderLust/classRoom/server.js
@@ -3,28 +3,45 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 // const cookieParser = require("cookie-parser");
 
 app.listen(3000, () => {
   console.log("Server is listening to 3000");
 });
 
-app.use(
-  session({
-    secret: "mysupersecretstring",
+app.use(flash());
+
+const sessionOptions = {
+  secret: "mysupersecretstring",
     resave: false,
     saveUninitialized: true,
-  })
-);
+}
 
-app.get("/reqcount", (req, res) => {
-  if(req.session.count){
-    req.session.count++;
-  }else {
-    req.session.count = 1;
+app.use(session(sessionOptions));
+
+app.get("/register", (req, res) => {
+  let {name = "anonymous"} = req.query;
+  req.session.name = name;
+  if(name === "anonymous"){
+    req.flash("error", "user not registred");
   }
-  res.send(`You sent a request ${req.session.count} times`);
-});
+  else req.flash("success","user registered successfully");
+  res.redirect("/hello");
+})
+
+app.get("/hello", (req, res) => {
+  res.render("page.ejs", {name : req.session.name});
+})
+
+// app.get("/reqcount", (req, res) => {
+//   if(req.session.count){
+//     req.session.count++;
+//   }else {
+//     req.session.count = 1;
+//   }
+//   res.send(`You sent a request ${req.session.count} times`);
+// });
 
 // app.get("/test", (req, res) => {
 //   res.send("test successful!");
